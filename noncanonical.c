00e73836d5e439b6f1af31a5912908e74c1b7175
Smigals <165923362+Smigals@users.noreply.github.com>
2024-05-22 01:17:17 +0100

    Add files via upload

--- noncanonical.c
+++ noncanonical.c
@@ -10,19 +10,51 @@
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
+#define START 0
+#define FLAG_RCV 1
+#define A_RCV 2
+#define C_RCV 3
+#define BCC1_OK 4
+#define STOPP 5
+#define C_RCV_I0 6
+#define C_RCV_I1 7
+#define DADOS 8
+#define BCC2_OK 9
+#define F 0x5c
+#define A 0x03
+#define SET 0x08
+#define UA  0x06
+#define RR0 0x01
+#define RR1 0x11
+#define REJ0    0x05
+#define REJ1    0x15
+#define I0  0x80
+#define I1  0xc0
+#define DISC    0x0a
+#define C_RCV_SET 10
+#define C_RCV_DISC 11
+#define C_RCV_RR0 12
+#define C_RCV_RR1 13
+#define C_RCV_REJ0 14
+#define C_RCV_REJ1 15
+#define C_RCV_UA 16
+#define ESC_OCT1 0x5d
+#define ESC_OCT2 0x7d
+#define ESC_OCT3 0x7c
+#define ESC_OCT4 0x5c
 
-volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
 {
-    int fd,c, res;
+    int fd,c, res, state;
+    unsigned char octet;
     struct termios oldtio,newtio;
-    char buf[255];
+    //unsigned char buf[255];
 
     if ( (argc < 2) ||
-         ((strcmp("/dev/ttyS0", argv[1])!=0) &&
-          (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+         ((strcmp("/dev/ttyS10", argv[1])!=0) &&
+          (strcmp("/dev/ttyS11", argv[1])!=0) )) {
+        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS11\n");
         exit(1);
     }
 
@@ -50,7 +82,7 @@ int main(int argc, char** argv)
     newtio.c_lflag = 0;
 
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
     /*
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
@@ -66,16 +98,391 @@ int main(int argc, char** argv)
     }
 
     printf("New termios structure set\n");
+    //Máquina de estados recetor Open//
+    unsigned char BCC1, BCC2;
+    int RespostaR=0;
+    state = START;
+    //LER SET
+    while (state!=STOPP) {       /* loop for input */
+       unsigned char buf[100];
+       res = read(fd,buf,1);   /* returns after 5 chars have been input */
+       // buf[res]=0;               /* so we can printf... */
+       printf("%02X \n", buf[0]);
+        
+       switch(state){
+            case START:
+                if (buf[0] == F){
+                    state=FLAG_RCV;
+                    printf("F=%02x ", buf[0]);
+                    fflush(stdout);
+                }
+                else {
+                    state=START;
+                    
+                } break;
+            case FLAG_RCV:
+                if (buf[0] == A){
+                    state=A_RCV; 
+                    printf("A=%02x ", buf[0]);  
+                    fflush(stdout);  
+                }
+                else if(buf[0]==F){
+                    state=FLAG_RCV;
+                    
+                }
+                else {
+                    state=START;
+                    
+                } break;
+            case A_RCV:
+                if (buf[0]==SET){
+                    state=C_RCV_SET;
+                    BCC1=A^SET;
+                    printf("C=%02x ", buf[0]);
+                    fflush(stdout);
+                    
+                }
+                /*else if(buf[0]==DISC){
+                    state=C_RCV_DISC;
+                    BCC1=A^DISC;
+                    
+                }
+                else if(buf[0]==I0){
+                    state=C_RCV_I0;
+                    BCC1=A^I0;
+                    
+                }
+                else if(buf[0]==I1){
+                    state=C_RCV_I1;
+                    BCC1=A^I1;
+                } 
+                else if(buf[0]==UA){
+                    state=C_RCV_UA;
+                    BCC1=A^UA;
+                }*/
+                else if(buf[0]==F){
+                    state=FLAG_RCV;
+                   
+                }   
+                else {
+                    state=START;
+                } break;
 
-    while (STOP==FALSE) {       /* loop for input */
-        res = read(fd,buf,5);   /* returns after 5 chars have been input */
-        buf[res]=0;               /* so we can printf... */
-        printf(":%x:%d\n", buf, res);
-        if (buf[0]=='z') STOP=TRUE;
+            case (C_RCV_SET /*|| C_RCV_DISC || C_RCV_UA*/):
+                if(buf[0]==BCC1){
+                    state=BCC1_OK;
+                    printf("BCC=%02x ", buf[0]);
+                    fflush(stdout);
+                }
+                else if(buf[0]==F){
+                    state=FLAG_RCV;
+                }               
+                else {
+                    state=START;
+          
+                } break;
+            case BCC1_OK:
+                if (buf[0]==F){
+                    state=STOPP;
+                    printf("FLAG=%02x\n", buf[0]);
+                    fflush(stdout);
+                }
+                else {
+                    state=START;
+                   
+                } break;
+        }
     }
 
+    //ENVIAR UA
+    unsigned char buf2[5]={0x5c, 0x01, 0x06, 0x01^0x06, 0x5c};
+    res=write(fd, buf2, 5);
+    printf("\n\n%d\n", res);
+    for(int i=0; i<5; i++) {
+        printf("%02x\n", buf2[i]);
+    
 
+    }
+    int detecao;
+    state=START;
+    //LER I
+    while (state!=STOPP) {       /* loop for input */
+       unsigned char buf3[100];
+       res = read(fd,buf3,1);   /* returns after 5 chars have been input */
+       // buf[res]=0;               /* so we can printf... */
+       printf("%02X \n", buf3[0]);
+        
+        char armazem;
+        switch(state){
+            case START:
+                if (buf3[0] == F){
+                    state=FLAG_RCV;
+                    printf("F=%02x ", buf3[0]);
+                }
+                else {
+                    state=START;
+                    
+                } break;
+            case FLAG_RCV:
+                if (buf3[0] == A){
+                    state=A_RCV; 
+                    printf("A=%02x ", buf3[0]); 
+                }
+                else if(buf3[0]==F){
+                    state=FLAG_RCV;
+                    
+                }
+                else {
+                    state=START;
+                    
+                } break;
+            case A_RCV:
+                /*if (buf3[0]==SET){
+                    state=C_RCV_SET;
+                    BCC1=A^SET;
+                    
+                }
+                else if(buf3[0]==DISC){
+                    state=C_RCV_DISC;
+                    BCC1=A^DISC;
+                    
+                }
+                */
+                if(buf3[0]==I0){
+                    state=C_RCV_I0;
+                    BCC1=A^I0;
+                    RespostaR=1;
+                    printf("C=%02x ", buf3[0]);
 
+                }
+                else if(buf3[0]==I1){
+                    state=C_RCV_I1;
+                    BCC1=A^I1;
+                    RespostaR=0;
+                    printf("C=%02x ", buf3[0]);
+                } 
+                /*else if(buf3[0]==UA){
+                    state=C_RCV_UA;
+                    BCC1=A^UA;
+                } */ 
+                else if(buf3[0]==F){
+                    state=FLAG_RCV;
+                   
+                }   
+                else {
+                    state=START;
+                } break;
+
+            case (C_RCV_I0):
+                if(buf3[0]==BCC1){
+                    state=BCC1_OK;
+                    printf("BCC=%02x ", buf3[0]);
+                }
+                else if(buf3[0]==F){
+                    state=FLAG_RCV;
+                }               
+                else {
+                    state=START;
+          
+                } break;
+            case (C_RCV_I1):
+                if(buf3[0]==BCC1){
+                    state=BCC1_OK;
+                    printf("BCC1=%02x\n", buf3[0]);
+                }
+                else if(buf3[0]==F){
+                    state=FLAG_RCV;
+                }               
+                else {
+                    state=START;
+          
+                } break;
+            case BCC1_OK:
+                if (buf3[0]==ESC_OCT1){
+                    state = BCC1_OK;
+                    detecao=1;
+                }
+                else if (buf3[0]==ESC_OCT3 && detecao == 1)
+                {
+                    BCC2=BCC2^ESC_OCT4;
+                    state = BCC1_OK;
+                    detecao=0;
+                }
+                else if (buf3[0]==ESC_OCT2 && detecao == 1)
+                {
+                    BCC2=BCC2^ESC_OCT1;
+                    state = BCC1_OK;
+                    detecao=0;
+                }
+                else if(buf3[0]==BCC2){
+                    state=BCC2_OK;
+                    printf("BCC2=%02x\n", buf3[0]);
+                }
+                else if(buf3[0]==F){
+                    state=FLAG_RCV;
+                }
+                else {
+                    octet = buf3[0];
+                    BCC2=octet^BCC2;
+                    state=BCC1_OK;
+                    //fazer xores com valores anteriores
+                } break;
+            case BCC2_OK:
+                if (buf3[0]==F)
+                {
+                    state = STOPP;
+                }
+                else{
+                    state=START;
+                }
+        }
+    }
+    char controloResposta=0;
+    if (RespostaR==1)
+    {
+        controloResposta=RR1;
+    }
+    else if (RespostaR==0)
+    {
+        controloResposta=RR0;
+    }
+    
+    //ENVIAR RRs
+    unsigned char buf4[5]={0x5c, 0x01, controloResposta, 0x01^controloResposta, 0x5c};
+    res=write(fd, buf4, 5);
+    printf("\n\n%d\n", res);
+    for(int i=0; i<5; i++) {
+    printf("%02x\n", buf4[i]);
+    }
+    state = START;
+    //RECEBER DISC
+    while (state!=STOPP) {       /* loop for input */
+        unsigned char buf5[1];
+        res = read(fd,buf5,1);   /* returns after 5 chars have been input */
+        buf5[res]=0;               /* so we can printf... */
+       
+        
+        switch(state){
+            case START:
+                if (buf5[0] == F){
+                    state=FLAG_RCV;  
+                    printf("F=%02x\n", buf5[0]);                 
+                }
+                else {
+                    state=START;
+                } break;
+            case FLAG_RCV:
+                if (buf5[0] == A){
+                    state=A_RCV;    
+                    printf("A=%02x\n", buf5[0]);  
+                }
+                else if(buf5[0]==F){
+                    state=FLAG_RCV;
+                }
+                else {
+                    state=START;
+                } break;
+            case A_RCV:
+                if (buf5[0]==DISC){
+                    state=C_RCV_DISC;
+                    printf("C=%02x\n", buf5[0]);
+                }
+                else if(buf5[0]==F){
+                    state=FLAG_RCV;
+                }
+                else {
+                    state=START;
+                } break;
+            case C_RCV_DISC:
+                if (buf5[0]==BCC1){
+                    state=BCC1_OK;
+                    printf("BCC=%02x\n", buf5[0]);
+                }
+                else if(buf5[0]==F){
+                    state=FLAG_RCV;
+                    printf("FLAG=%02x\n", buf5[0]);
+                }
+                else {
+                    state=START;
+                } break;
+            case BCC1_OK:
+                if(buf5[0]==F){
+                    state=STOPP;
+                }
+                else {
+                    state=START;
+                } break;
+        }
+        
+    }
+    //ENVIAR DISC
+     unsigned char buf6[5] ={0x5c, 0x01, DISC, 0x01^DISC,0x5c};
+    res = write(fd,buf6,5);
+    printf("%d ", res);
+    for(int i=0; i<5;i++){
+        printf("%02X ", buf6[i]);
+    }
+
+    //LER UA
+    state = START;
+    while (state!=STOPP) {       /* loop for input */
+      unsigned char buf7[1];
+       res = read(fd,buf7,1);   /* returns after 5 chars have been input */
+       buf7[res]=0;               /* so we can printf... */
+       
+        
+       switch(state){
+            case START:
+                if (buf7[0] == F){
+                    state=FLAG_RCV;
+                    printf("F=%02x\n", buf7[0]);
+                }
+                else {
+                    state=START;
+                } break;
+            case FLAG_RCV:
+                if (buf7[0] == A){
+                    state=A_RCV; 
+                    printf("A=%02x\n", buf7[0]);
+                }
+                else if(buf2[0]==F){
+                    state=FLAG_RCV;
+                }
+                else {
+                    state=START;
+                } break;
+            case A_RCV:
+                if (buf7[0]==UA){
+                    state=C_RCV;
+                    printf("C=%02x\n", buf7[0]);
+                }
+                else if(buf7[0]==F){
+                    state=FLAG_RCV;
+                }
+                else {
+                    state=START;
+                } break;
+            case C_RCV:
+                if (buf7[0]==BCC1){
+                    state=BCC1_OK;
+                    printf("BCC=%02x\n", buf7[0]);
+                }
+                else if(buf7[0]==F){
+                    state=FLAG_RCV;
+                    printf("FLAG=%02x\n", buf7[0]);
+                }
+                else {
+                    state=START;
+                } break;
+            case BCC1_OK:
+                if(buf7[0]==F){
+                    state=STOPP;
+                }
+                else {
+                    state=START;
+                } break;
+        }    
+    }
     /*
     O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião
     */
--- writenoncanonical.c
+++ writenoncanonical.c
@@ -11,23 +11,48 @@
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
-
+#define START 0
+#define FLAG_RCV 1
+#define A_RCV 2
+#define C_RCV 3
+#define BCC1_OK 4
+#define STOPP 5
+#define F 0x5c
+#define A 0x01
+#define SET 0x08
+#define UA  0x06
+#define RR0 0x01
+#define RR1 0x11
+#define REJ0    0x05
+#define REJ1    0x15
+#define I0  0x80
+#define I1  0xc0
+#define DISC    0x0a
+#define C_RCV_SET 10
+#define C_RCV_DISC 11
+#define C_RCV_RR0 12
+#define C_RCV_RR1 13
+#define C_RCV_REJ0 14
+#define C_RCV_REJ1 15
+#define ESC_OCT1 0x5d
+#define ESC_OCT2 0x7c
+#define ESC_OCT3 0x7d
 volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
 {
-    int fd,c, res;
+    int fd,c, res, state;
+    unsigned char BCC1;
     struct termios oldtio,newtio;
     //char buf[255];
     int i, sum = 0, speed = 0;
 
     if ( (argc < 2) ||
-         ((strcmp("/dev/ttyS0", argv[1])!=0) &&
-          (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+         ((strcmp("/dev/ttyS10", argv[1])!=0) &&
+          (strcmp("/dev/ttyS11", argv[1])!=0) )) {
+        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS11\n");
         exit(1);
     }
-
 
     /*
     Open serial port device for reading and writing and not as controlling tty
@@ -77,28 +102,354 @@ int main(int argc, char** argv)
         buf[i] = 'a';
     }*/
    
-    unsigned char buf[5] ={0x5c, 0x03, 0x08, 0x00, 0x5c};
-    buf[4]=buf[2]^buf[3];
-    /*testing*/
-    buf[25] = '\n';
-
+    unsigned char buf[5] ={0x5c, 0x03, 0x08, 0x03^0x08,0x5c};
     res = write(fd,buf,5);
-    sleep(1);
-    printf("%d bytes written\n", res);
+    printf("%d ", res);
+    for(int i=0; i<5;i++){
+        printf("%02X ", buf[i]);
+    }
+    
+    /*testing*/    
 
-
+    printf("\n%d bytes written\n", res);
+    //ENVIO DO SET
+    state = START;
+    while (state!=STOPP) {       /* loop for input */
+      unsigned char buf2[1];
+       res = read(fd,buf2,1);   /* returns after 5 chars have been input */
+       buf2[res]=0;               /* so we can printf... */
+       
+        
+       switch(state){
+            case START:
+                if (buf2[0] == F){
+                    state=FLAG_RCV;
+                    printf("F=%02x ", buf2[0]);
+                    fflush(stdout);
+                }
+                else {
+                    state=START;
+                    //printf("F=%02x\n", buf2[0]);
+                } break;
+            case FLAG_RCV:
+                if (buf2[0] == A){
+                    state=A_RCV; 
+                    printf("A=%02x ", buf2[0]);
+                    fflush(stdout);      
+                }
+                else if(buf2[0]==F){
+                    state=FLAG_RCV;
+                    
+                }
+                else {
+                    state=START;
+                    
+                } break;
+            case A_RCV:
+                if (buf2[0]==UA){
+                    state=C_RCV;
+                    printf("C=%02x ", buf2[0]);
+                    fflush(stdout);
+                    BCC1=A^UA;
+                }
+                else if(buf2[0]==F){
+                    state=FLAG_RCV;
+                   
+                }
+                else {
+                    state=START;
+                   
+                } break;
+            case C_RCV:
+                if (buf2[0]==BCC1){
+                    state=BCC1_OK;
+                    printf("BCC=%02x ", buf2[0]);
+                    fflush(stdout);
+                }
+                else if(buf2[0]==F){
+                    state=FLAG_RCV;
+                    
+                }
+                else {
+                    state=START;
+                    
+                } break;
+            case BCC1_OK:
+                if(buf2[0]==F){
+                    state=STOPP;
+                    printf("FLAG=%02x\n", buf2[0]);
+                    fflush(stdout);
+                }
+                else {
+                    state=START;
+                    
+                } break;
+        }
+        
+    }
+   
     /*
     O ciclo FOR e as instruções seguintes devem ser alterados de modo a respeitar
     o indicado no guião
     */
+    //dados
+    unsigned char buf3[10] ={0x5c, 0x03, 0x80, 0x03^0x80, 0x9b, 0x61, 0xa1, 0xff, ((0x9b^0x61)^0xa1)^0xff,0x5c};
+    unsigned char buffer[11];
+    int tam=0,j=0, a=0;
     
+    while (buf3[j]!=NULL){
+        if(j!=0 || j!=10){
+            if(strcmp(buf3[j],F)==0)
+            {
+                buffer[a]=ESC_OCT1;
+                buffer[a+1]=ESC_OCT2;
+                a++;
+            }
+            else if (strcmp(buf3[j],ESC_OCT1)==0)
+            {
+                buffer[a]=ESC_OCT1;
+                buffer[a+1]=ESC_OCT3;
+                a++;
+            }
+        }
+        buffer[a]=buf3[j];
+        j++;
+        a++;
+    }
 
+    res = write(fd,buffer,11);
+    for(int i=0; i<11;i++){
+        printf("%02X ", buffer[i]);
+    }
+
+    state=START;
+    //RR
+    while (state!=STOPP) {       /* loop for input */
+       unsigned char buf4[100];
+       res = read(fd,buf4,1);   /* returns after 5 chars have been input */
+       // buf[res]=0;               /* so we can printf... */
+       printf("%02X \n", buf4[0]);
+        switch(state){
+            case (START):
+                if (buf4[0] == F){
+                    state=FLAG_RCV;
+                    printf("F=%02x ", buf4[0]);
+                   
+                }
+                else {
+                    state=START;
+                    
+                } break;
+            case (FLAG_RCV):
+                if (buf4[0] == A){
+                    state=A_RCV; 
+                    printf("A=%02x ", buf4[0]);
+                 
+                        
+                }
+                else if(buf4[0]==F){
+                    state=FLAG_RCV;
+                    
+                }
+                else {
+                    state=START;
+                    
+                } break;
+            case (A_RCV):
+                /*if (buf4[0]==SET){
+                    state=C_RCV_SET;
+                    BCC1=A^SET;
+                    
+                }
+                else if(buf4[0]==DISC){
+                    state=C_RCV_DISC;
+                    BCC1=A^DISC;
+                    
+                }
+                */
+                if(buf4[0]==RR0){
+                    state=C_RCV_RR0;
+                    BCC1=A^RR0;
+                    printf("C=%02x ", buf4[0]);
+                
+                }
+                else if(buf4[0]==RR1){
+                    state=C_RCV_RR1;
+                    BCC1=A^RR1;
+                    printf("C=%02x ", buf4[0]);
+                
+                } 
+                else if(buf4[0]==REJ0){
+                    state=C_RCV_REJ0;
+                    BCC1=A^REJ0;
+                    printf("C=%02x ", buf4[0]);
+                 
+                
+                } 
+                else if(buf4[0]==REJ1){
+                    state=C_RCV_REJ1;
+                    BCC1=A^REJ1;
+                    printf("C=%02x ", buf4[0]);
+      
+                } 
+                /*else if(buf4[0]==UA){
+                    state=C_RCV_UA;
+                    BCC1=A^UA;
+                } */ 
+                else if(buf4[0]==F){
+                    state=FLAG_RCV;
+                   
+                }   
+                else {
+                    state=START;
+                } break;
+
+            case (C_RCV_RR0):
+                if(buf4[0]==BCC1){
+                    state=BCC1_OK;
+                    printf("BCC=%02x ", buf4[0]);
+                    
+                }
+                else if(buf4[0]==F){
+                    state=FLAG_RCV;
+                }               
+                else {
+                    state=START;
+          
+                } break;
+            case (C_RCV_RR1):
+                if(buf4[0]==BCC1){
+                    state=BCC1_OK;
+                    printf("BCC=%02x ", buf4[0]);
+                }
+                else if(buf4[0]==F){
+                    state=FLAG_RCV;
+                }               
+                else {
+                    state=START;
+          
+                } break;
+            case (C_RCV_REJ0):
+                if(buf4[0]==BCC1){
+                    state=BCC1_OK;
+                    printf("BCC=%02x ", buf4[0]);
+                    
+                }
+                else if(buf4[0]==F){
+                    state=FLAG_RCV;
+                }               
+                else {
+                    state=START;
+          
+                } break;
+            case (C_RCV_REJ1):
+                if(buf4[0]==BCC1){
+                    state=BCC1_OK;
+                    printf("BCC=%02x\n", buf4[0]);
+                    
+                }
+                else if(buf4[0]==F){
+                    state=FLAG_RCV;
+                }               
+                else {
+                    state=START;
+          
+                } break;
+            case (BCC1_OK):
+                if(buf4[0]==F){
+                    state=STOPP;    
+                }
+                else {
+                    state=START;
+                } break;
+        }
+    }
+
+    //disc
+    unsigned char buf5[5] ={0x5c, 0x03, DISC, 0x03^DISC,0x5c};
+    res = write(fd,buf5,5);
+    printf("%d ", res);
+    for(int i=0; i<5;i++){
+        printf("%02X ", buf5[i]);
+    }
+
+    state = START;
+    //receber disc
+    while (state!=STOPP) {       /* loop for input */
+        unsigned char buf6[1];
+        res = read(fd,buf6,1);   /* returns after 5 chars have been input */
+        buf6[res]=0;               /* so we can printf... */
+       
+        
+        switch(state){
+            case (START):
+                if (buf6[0] == F){
+                    state=FLAG_RCV;  
+                    printf("F=%02x ", buf6[0]);                 
+                }
+                else {
+                    state=START;
+                } break;
+            case FLAG_RCV:
+                if (buf6[0] == A){
+                    state=A_RCV;    
+                    printf("A=%02x ", buf6[0]);  
+                }
+                else if(buf6[0]==F){
+                    state=FLAG_RCV;
+                }
+                else {
+                    state=START;
+                } break;
+            case (A_RCV):
+                if (buf6[0]==DISC){
+                    state=C_RCV_DISC;
+                    printf("C=%02x ", buf6[0]);
+                }
+                else if(buf6[0]==F){
+                    state=FLAG_RCV;
+                }
+                else {
+                    state=START;
+                } break;
+            case C_RCV_DISC:
+                if (buf6[0]==BCC1){
+                    state=BCC1_OK;
+                    printf("BCC=%02x\n", buf6[0]);
+                }
+                else if(buf6[0]==F){
+                    state=FLAG_RCV;
+                }
+                else {
+                    state=START;
+                } break;
+            case (BCC1_OK):
+                if(buf6[0]==F){
+                    state=STOPP;
+                }
+                else {
+                    state=START;
+                } break;
+        }
+        
+    }
+
+    //enviar UA
+    unsigned char buf7[5] ={0x5c, 0x03, UA, 0x03^UA,0x5c};
+        res = write(fd,buf7,5);
+        printf("%d ", res);
+        for(int i=0; i<5;i++){
+            printf("%02X ", buf7[i]);
+        }
+
+    sleep(1);
+    
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
         perror("tcsetattr");
         exit(-1);
     }
 
-
+	
     close(fd);
     return 0;
 }
